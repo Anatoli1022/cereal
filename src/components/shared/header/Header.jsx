8e310834dcb47bc1cb61398ea29d19f86b79d881
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-09-16 12:56:44 +0300

    feat: new meta description, button aria label  in header

--- src/components/shared/header/Header.jsx
+++ src/components/shared/header/Header.jsx
@@ -7,11 +7,15 @@ const cx = classNames.bind(styles);
 const Header = () => {
   return (
     <header className={cx('header')}>
-      <div className={cx('container','header-container')}>
+      <div className={cx('container', 'header-container')}>
         <nav className={cx('navigation')}>
-          <Link href="/" className={cx('link')}>cereal</Link>
-          <Link href="/" className={cx('link')}>design</Link>
-          <button className={cx('button')}></button>
+          <Link href='/' className={cx('link')}>
+            cereal
+          </Link>
+          <Link href='/' className={cx('link')}>
+            design
+          </Link>
+          <button type='button' className={cx('button')} aria-label='Button open menu'></button>
         </nav>
       </div>
     </header>
