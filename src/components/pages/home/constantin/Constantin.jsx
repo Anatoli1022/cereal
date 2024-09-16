9fd73f00f3cb4eda6942d989c4067ca1e19f52ae
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-09-16 13:50:18 +0300

    feat: adds mobile version constantin,space, sometimes,global,container

--- src/components/pages/home/constantin/Constantin.jsx
+++ src/components/pages/home/constantin/Constantin.jsx
@@ -11,30 +11,23 @@ const Constantin = () => {
         <h2 className={cx('title')}>Constantin Brâncuși</h2>
         <div className={cx('wrapper-text')}>
           <p>
-            It is strange to think that the items around us will soon be hidden
-            from sight. Design masterpieces abound: a desk by the now-fabled
-            Cuban-born designer Clara Porset sits near a thick woollen rug.
+            It is strange to think that the items around us will soon be hidden from sight. Design masterpieces abound:
+            a desk by the now-fabled Cuban-born designer Clara Porset sits near a thick woollen rug.
           </p>
           <p className={cx('text-right')}>
-            made by the family who wove for the Mexican architect Luís Barragán.
-            Gifts from <br /> artist friends and colleagues are dotted between
-            sculptures created by Wu’s husband, sculptor Alma Allen, whose
-            abstract creations might be likened to the <br /> early-20th-century
-            forms of Constantin Brâncuși.
+            made by the family who wove for the Mexican architect Luís Barragán. Gifts from <br /> artist friends and
+            colleagues are dotted between sculptures created by Wu’s husband, sculptor Alma Allen, whose abstract
+            creations might be likened to the <br /> early-20th-century forms of Constantin Brâncuși.
           </p>
         </div>
         <ul className={cx('list')}>
           <li>
-            <img src={windowImage} alt="" loading="lazy" />
-            <p className={cx('image-text')}>
-              Beside the home’s present, Wu is conscious of its past.
-            </p>
+            <img src={windowImage} alt='' loading='lazy' className={cx('image')} />
+            <p className={cx('image-text')}>Beside the home’s present, Wu is conscious of its past.</p>
           </li>
           <li>
-            <img src={lamp} alt="" />
-            <p className={cx('image-text')}>
-              the Mexican architect Luís Barragán. Gifts from artist friends
-            </p>
+            <img src={lamp} alt='' loading='lazy' className={cx('image')} />
+            <p className={cx('image-text')}>the Mexican architect Luís Barragán. Gifts from artist friends</p>
           </li>
         </ul>
       </div>
